7e662ce889dca5ab7a44ceae76ccebaa1c612f09
libyano93ly <younus93ly@gmail.com>
2022-07-28 09:47:18 +0200

    Add IP Class

--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -6,7 +6,26 @@ namespace Indexers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ip = new IP(198,168,1,1);
+            Console.WriteLine(ip.Address);
+
+
+            Console.ReadKey();
         }
+    }
+
+    public class IP
+    {
+        private int[] segments = new int[4];
+
+        public IP(int segments1, int segments2, int segments3, int segments4)
+        {
+           segments[0] =segments1;
+           segments[1] =segments2;
+           segments[2] =segments3;
+           segments[3] =segments4;
+        }
+
+        public string Address => string.Join(".", segments);
     }
 }
