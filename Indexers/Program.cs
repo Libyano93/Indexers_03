36f4c683098061a3f2be597ed605ef93860d3581
libyano93ly <younus93ly@gmail.com>
2022-07-28 09:56:35 +0200

    Using Index With IP

--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -6,8 +6,12 @@ namespace Indexers
     {
         static void Main(string[] args)
         {
-            var ip = new IP(198,168,1,1);
-            Console.WriteLine(ip.Address);
+            var ip = new IP(198, 168, 1, 1);
+
+            var firstSegments = ip[0];
+
+            Console.WriteLine($"IP ADDRESS: {ip.Address}");
+            Console.WriteLine($"FIRST SEGMENTS: {firstSegments}");
 
 
             Console.ReadKey();
@@ -15,15 +19,27 @@ namespace Indexers
     }
 
     public class IP
-    {
+    {// Using Index With IP
         private int[] segments = new int[4];
 
+        public int this[int index]
+        {
+            get
+            {
+                return segments[index];
+            }
+
+            set
+            {
+                segments[index] = value;
+            }
+        }
         public IP(int segments1, int segments2, int segments3, int segments4)
         {
-           segments[0] =segments1;
-           segments[1] =segments2;
-           segments[2] =segments3;
-           segments[3] =segments4;
+            segments[0] = segments1;
+            segments[1] = segments2;
+            segments[2] = segments3;
+            segments[3] = segments4;
         }
 
         public string Address => string.Join(".", segments);
